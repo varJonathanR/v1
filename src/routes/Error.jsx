56634a803c3fb9e1fdc33843482e1b08e2beb9c3
Jonathan Rodriguez <jonathan20.rodriguez04@gmail.com>
2023-08-24 18:03:08 -0600

    Error landing page added

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Error() {
   return (
-    <div className='container'>Ops!, Page not found :(!</div>
+    <div className='container centered-content'>
+      <h1>WHOOPS!</h1>
+      <p>Something went wrong. This is embarrasing.</p>
+      <Link to="/" className='buttonA'>
+        GO HOME
+      </Link>
+    </div>
   )
 }
